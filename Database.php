e082209ab8089b921751568c3ee7df231fb554c5
proeiffel <45282718+proeiffel@users.noreply.github.com>
2024-11-19 20:18:25 +0300

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db = 'levorg';
+    private $user = 'root';
+    private $pass = '';
+    private $charset = 'utf8mb4';
+    private $pdo;
+
+    public function __construct() {
+        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        $this->pdo = new PDO($dsn, $this->user, $this->pass);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
